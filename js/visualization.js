916b88b17affad568ca1bed9f9c6e8a4109f64f8
yuh2k <yuhang20518@gmail.com>
2023-11-25 18:13:32 -0500

    new england map position

--- js/visualization.js
+++ js/visualization.js
@@ -1,24 +1,24 @@
 
-  const width = 1000;
-        const height = 750;
+const width = 1000;
+const height = 750;
 
-        const svg = d3.select(".vis-holder").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+const svg = d3.select(".vis-holder").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-        const projection = d3.geoAlbersUsa()
-            .translate([-400, height / 2 + 400])
-            .scale(3000);
+const projection = d3.geoAlbersUsa()
+    .translate([-400, 700])
+    .scale(3000);
 
-        const path = d3.geoPath().projection(projection);
+const path = d3.geoPath().projection(projection);
 
-        d3.json("data/2023.json").then(function(topojsonData) {
-            const geojsonData = topojson.feature(topojsonData, topojsonData.objects.states); // 使用 TopoJSON 的方法转换
+d3.json("data/2023.json").then(function(topojsonData) {
+    const geojsonData = topojson.feature(topojsonData, topojsonData.objects.states); // 使用 TopoJSON 的方法转换
 
-            svg.selectAll("path")
-                .data(geojsonData.features)
-                .enter().append("path")
-                .attr("d", path)
-                .attr("fill", "steelblue")
-                .attr("stroke", "white");
-        });
+    svg.selectAll("path")
+        .data(geojsonData.features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("fill", "steelblue")
+        .attr("stroke", "white");
+});
