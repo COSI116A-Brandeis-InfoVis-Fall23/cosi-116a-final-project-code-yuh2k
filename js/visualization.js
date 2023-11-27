8083346b2d148a51795dadab43dde01018351818
hahaha <hahaha@hahahadeMacBook-Pro.local>
2023-11-26 22:46:06 -0800

    add color to the state map

--- js/visualization.js
+++ js/visualization.js
@@ -1,4 +1,3 @@
-
 const width = 1000;
 const height = 750;
 
@@ -12,13 +11,41 @@ const projection = d3.geoAlbersUsa()
 
 const path = d3.geoPath().projection(projection);
 
-d3.json("data/2023.json").then(function(topojsonData) {
-    const geojsonData = topojson.feature(topojsonData, topojsonData.objects.states); // 使用 TopoJSON 的方法转换
+const data1 = {
+    "Vermont": 10,
+    "New Hampshire": 50,
+    "Maine": 109,
+    "Rhode Island": 93,
+    "Connecticut": 344,
+    "Massachusetts": 496
+};
 
+const getName = d => d.properties.STATENAM;
+
+function getColor(dataValue) {
+    if (dataValue < 50) {
+        return "blue";
+    } else if (dataValue < 200) {
+        return "green";
+    } else if (dataValue < 400) {
+        return "yellow";
+    } else {
+        return "red";
+    }
+}
+d3.json("data/2023.json").then(function(topojsonData) {
+    const geojsonData = topojson.feature(topojsonData, topojsonData.objects.states);
+    geojsonData.features.forEach(function(d) {
+        console.log(getName(d));
+    });
     svg.selectAll("path")
         .data(geojsonData.features)
         .enter().append("path")
         .attr("d", path)
-        .attr("fill", "steelblue")
+        .attr("fill", function(d) {
+            const regionName = getName(d);
+            const dataValue = data1[regionName] || 0; 
+            return getColor(dataValue);
+        })
         .attr("stroke", "white");
 });
